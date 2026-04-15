974c0df7c92369d41430b4f564c51afcd84188af
Jose Romero <josedavisromerogutierrez@gmail.com>
2026-04-15 04:22:13 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'josedavisromerogutierrez-collab/Web-Dev-2',
+});
