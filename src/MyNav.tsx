0d9c081427fa15e18468c724c927b0812c66ae68
Jose Romero <josedavisromerogutierrez@gmail.com>
2026-04-20 01:45:26 -0400

    Added form

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -1,6 +1,7 @@
 import Home from "./home";
 import About from "./About";
 import Products from "./Products";
+import MyForm from "./MyForm";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function MyNav() {
@@ -12,6 +13,7 @@ function MyNav() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/form" element={<MyForm />} />
             </Routes>
         </BrowserRouter>
     );
@@ -24,6 +26,7 @@ function SetNav() {
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/products">Products</Link></li>
+                <li><Link to="/form">Form</Link></li>
             </ul>
         </nav>
     );
